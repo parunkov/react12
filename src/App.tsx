a0de5cb0f030981d589989e46961a208cd33c111
parunkov <parunkov@gmail.com>
2024-04-16 10:20:12 +0500

    progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -12,57 +12,78 @@ export interface ItestData {
   type?: string;
   result?: string | string[] | null;
   callback?: (value: string | string[]) => void;
+  active?: boolean;
+  selected?: boolean;
 }
 
 interface IappState {
   time: number;
   curretIndex: number;
   test: ItestData[];
+  progress: number;
+  buffer: number;
 }
 
 function App() {
   const initialState: IappState = {
     time: 0,
     curretIndex: 0,
+    progress: 0,
+    buffer: 0,
     test: [
       {
         id: 1,
         type: 'radio',
         question: 'Что должен знать фронтенд-разработчик? Назовите три ключевых технологии',
         answers: ['HTML, CSS и JavaScript', 'Kotlin, PHP и Javascript', 'PHP, HTML и CSS'],
-        result: null
+        result: null,
+        active: true,
+        selected: false
       },
       {
         id: 2,
         type: 'checkbox',
         question: 'Отметьте варианты ответа',
         answers: ['Вариант 1', 'Вариант 2', 'Вариант 3'],
-        result: null
+        result: null,
+        active: false,
+        selected: false
       },
       {
         id: 3,
         type: 'input',
         question: 'Напишите ответ на вопрос',
         answers: [],
-        result: null
+        result: null,
+        active: false,
+        selected: false
       },
       {
         id: 4,
         type: 'textarea',
         question: 'Напишите ответ на вопрос и решение',
         answers: [],
-        result: null
+        result: null,
+        active: false,
+        selected: false
       }
     ]
   };
 
   const onButtonClick = (value: string | string[]) => {
     const newState: IappState = { ...appState };
-    // newState.curretIndex += 1;
-    newState.curretIndex = 3;
-    newState.test[appState.curretIndex].result = value;
-    setAppState(newState);
-    console.log(appState);
+    newState.curretIndex += 1;
+    // newState.curretIndex = 3;
+    if (newState.test[appState.curretIndex]) {
+      newState.test[appState.curretIndex].result = value;
+      newState.test[appState.curretIndex].active = false;
+      newState.test[appState.curretIndex].selected = true;
+      setAppState(newState);
+      console.log(appState);
+    }
+    if (newState.test[appState.curretIndex + 1]) {
+      newState.test[appState.curretIndex + 1].active = true;
+    }
   }
 
   const [appState, setAppState] = useState(initialState);
@@ -77,7 +98,9 @@ function App() {
         <h1>Тестирование</h1>
         <div className="clock"></div>
       </div>
-      <div className="progressWrapper"></div>
+      <div className="progressWrapper">
+        {appState.test.map((item) => <div key={item.id} className={`progressItem ${item.active ? 'active' : ''} ${item.selected ? 'selected' : ''}`}></div>)}
+      </div>
       <div className="testWrapper">
         {appState.test[appState.curretIndex]?.type === 'radio' ? <RadioTest
           question={appState.test[appState.curretIndex].question}
@@ -87,11 +110,11 @@ function App() {
           question={appState.test[appState.curretIndex].question}
           answers={appState.test[appState.curretIndex].answers}
           callback={onButtonClick} /> : ''}
-          {appState.test[appState.curretIndex]?.type === 'input' ? <InputTest
+        {appState.test[appState.curretIndex]?.type === 'input' ? <InputTest
           question={appState.test[appState.curretIndex].question}
           answers={appState.test[appState.curretIndex].answers}
           callback={onButtonClick} /> : ''}
-          {appState.test[appState.curretIndex]?.type === 'textarea' ? <TextareaTest
+        {appState.test[appState.curretIndex]?.type === 'textarea' ? <TextareaTest
           question={appState.test[appState.curretIndex].question}
           answers={appState.test[appState.curretIndex].answers}
           callback={onButtonClick} /> : ''}
